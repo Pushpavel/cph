6231c6ea453f613f4671525d613da0c3342ef0f7
Mechi <mechisama1@gmail.com>
2024-04-29 21:54:44 +0530

    adds empty intellij-plugin template

---
+++ others/plugins/intellij-plugin/src/main/kotlin/com/github/mechisama/cph/listeners/MyApplicationActivationListener.kt
@@ -0,0 +1,11 @@
+package com.github.mechisama.cph.listeners
+
+import com.intellij.openapi.application.ApplicationActivationListener
+import com.intellij.openapi.wm.IdeFrame
+
+internal class MyApplicationActivationListener : ApplicationActivationListener {
+
+    override fun applicationActivated(ideFrame: IdeFrame) {
+        
+    }
+}
