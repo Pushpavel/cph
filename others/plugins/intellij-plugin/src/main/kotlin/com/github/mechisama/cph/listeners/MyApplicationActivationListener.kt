2af7a51c41c017a4467a87c97a904ecfb3a35d22
Mechi <mechisama1@gmail.com>
2024-05-05 19:12:45 +0530

    [intellij-plugin] simple javalin server

--- others/plugins/intellij-plugin/src/main/kotlin/com/github/mechisama/cph/listeners/MyApplicationActivationListener.kt
+++ others/plugins/intellij-plugin/src/main/kotlin/com/github/mechisama/cph/listeners/MyApplicationActivationListener.kt
@@ -1,11 +1,36 @@
 package com.github.mechisama.cph.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.wm.IdeFrame
+import io.javalin.Javalin
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
 
 internal class MyApplicationActivationListener : ApplicationActivationListener {
-
+    private var done = false;
     override fun applicationActivated(ideFrame: IdeFrame) {
-        
+        if (done) return;
+        done = true;
+        Javalin.create(/*config*/)
+            .sse("/events/activeFile") { client ->
+                client.keepAlive()
+                ideFrame.project!!.messageBus.connect().subscribe(
+                    FileEditorManagerListener.FILE_EDITOR_MANAGER,
+                    object : FileEditorManagerListener {
+                        override fun selectionChanged(event: FileEditorManagerEvent) {
+                            println("activeFile: " + event.newFile.canonicalPath)
+                            client.sendEvent(
+                                Json.encodeToString(
+                                    event.newFile.canonicalPath
+                                )
+                            )
+                        }
+                    }
+                )
+
+            }
+            .start(5678)
     }
 }
