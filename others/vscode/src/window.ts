0a78479b3f0e04aa12ac196070b4824b99c5d9e8
Mechi <mechisama1@gmail.com>
2024-05-05 14:55:44 +0530

    change window as class from module, to support getters

--- others/vscode/src/window.ts
+++ others/vscode/src/window.ts
@@ -13,62 +13,64 @@ import { StatusBarItemImpl } from './impl/StatusBarItemImpl';
 import { EventEmitterImpl } from './impl/EventEmitterImpl';
 import { TextEditorImpl } from './impl/TextEditorImpl';
 import { ideClient } from './api/ide';
+import { registerWebviewViewProvider } from './api/webview';
 
-export { registerWebviewViewProvider } from './api/webview';
-
-// #hack
-export const activeTextEditor = undefined;
-
-export function showErrorMessage(msg: string) {
-    console.error('🌟 ERROR_MESSAGE:', msg);
-}
-
-export function showInfoMessage(msg: string) {
-    console.info('🌟 INFO_MESSAGE:', msg);
-}
-
-export async function showQuickPick(items: string[]) {
+class WindowImpl {
     // #hack
-    console.log('🌟 QUICK_PICK: showing', items, `Choosing ${items[0]}`);
-    return items[0];
+    activeTextEditor = undefined;
+
+    registerWebviewViewProvider = registerWebviewViewProvider;
+
+    showErrorMessage(msg: string) {
+        console.error('🌟 ERROR_MESSAGE:', msg);
+    }
+
+    showInfoMessage(msg: string) {
+        console.info('🌟 INFO_MESSAGE:', msg);
+    }
+
+    async showQuickPick(items: string[]) {
+        // #hack
+        console.log('🌟 QUICK_PICK: showing', items, `Choosing ${items[0]}`);
+        return items[0];
+    }
+
+    createStatusBarItem(
+        alignment?: StatusBarAlignment,
+        priority?: number,
+    ): StatusBarItem {
+        const StatusBarItem = new StatusBarItemImpl(alignment);
+        StatusBarItem.priority = priority;
+        return StatusBarItem;
+    }
+
+    // #hack
+    _onDidChangeVisibleTextEditorsEmitter = new EventEmitterImpl<
+        readonly TextEditor[]
+    >();
+
+    get onDidChangeActiveTextEditor() {
+        return ideClient.onDidChangeActiveTextEditor();
+    }
+    onDidChangeVisibleTextEditors: Event<readonly TextEditor[]> =
+        this._onDidChangeVisibleTextEditorsEmitter.event;
+
+    async showTextDocument(
+        document: TextDocument,
+        column?: ViewColumn,
+        preserveFocus?: boolean,
+    ): Promise<TextEditor> {
+        console.log(
+            `🌟 WINDOW: showing TEXT_EDITOR for document=${document.fileName} at column=${column} with`,
+            { preserveFocus },
+        );
+        return new TextEditorImpl();
+    }
+
+    createOutputChannel(name: string, languageId?: string): OutputChannel {
+        unusedWowo({ languageId });
+        return new OutputChannelImpl(name);
+    }
 }
 
-export function createStatusBarItem(
-    alignment?: StatusBarAlignment,
-    priority?: number,
-): StatusBarItem {
-    const StatusBarItem = new StatusBarItemImpl(alignment);
-    StatusBarItem.priority = priority;
-    return StatusBarItem;
-}
-
-// #hack
-const _onDidChangeVisibleTextEditorsEmitter = new EventEmitterImpl<
-    readonly TextEditor[]
->();
-
-export const onDidChangeActiveTextEditor =
-    ideClient.onDidChangeActiveTextEditor();
-
-export const onDidChangeVisibleTextEditors: Event<readonly TextEditor[]> =
-    _onDidChangeVisibleTextEditorsEmitter.event;
-
-export async function showTextDocument(
-    document: TextDocument,
-    column?: ViewColumn,
-    preserveFocus?: boolean,
-): Promise<TextEditor> {
-    console.log(
-        `🌟 WINDOW: showing TEXT_EDITOR for document=${document.fileName} at column=${column} with`,
-        { preserveFocus },
-    );
-    return new TextEditorImpl();
-}
-
-export function createOutputChannel(
-    name: string,
-    languageId?: string,
-): OutputChannel {
-    unusedWowo({ languageId });
-    return new OutputChannelImpl(name);
-}
+export default new WindowImpl();
