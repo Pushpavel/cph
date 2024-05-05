ca14e6edb5f19b35cf5a97f7910efe98eb5def10
Mechi <mechisama1@gmail.com>
2024-05-05 19:19:38 +0530

    change workspace as class from module, to support getters

--- others/vscode/src/index.ts
+++ others/vscode/src/index.ts
@@ -1,5 +1,5 @@
 export { default as window } from './window';
-export * as workspace from './workspace';
+export { default as workspace } from './workspace';
 export * as commands from './commands';
 export * as context from './context';
 export * as env from './env';
--- others/vscode/src/workspace.ts
+++ others/vscode/src/workspace.ts
@@ -11,52 +11,55 @@ import { EventEmitterImpl } from './impl/EventEmitterImpl';
 import { TextDocumentImpl } from './impl/TextDocumentImpl';
 // import { readFile } from 'fs/promises';
 
-export const workspaceFolders = [
-    {
-        uri: {
-            fsPath: process.env.HOME + '/github/cph/others/playground',
+class WorkspaceImpl {
+    workspaceFolders = [
+        {
+            uri: {
+                fsPath: process.env.HOME + '/github/cph/others/playground',
+            },
         },
-    },
-];
+    ];
 
-export async function openTextDocument(
-    fileName: string,
-): Promise<TextDocument> {
-    if (typeof fileName !== 'string') {
-        unimplementedWowo(
-            `
-            the original vscode.workspace.openTextDocument(...) can take args other than just a filepath string, it seems we are using those other signatures
-            https://github.com/microsoft/vscode/blob/73a2c100f6229b231d6b255e36d1789c25d92285/src/vs/workbench/api/common/extHost.api.impl.ts#L1018
-            `,
+    async openTextDocument(fileName: string): Promise<TextDocument> {
+        if (typeof fileName !== 'string') {
+            unimplementedWowo(
+                `
+                the original vscode.workspace.openTextDocument(...) can take args other than just a filepath string, it seems we are using those other signatures
+                https://github.com/microsoft/vscode/blob/73a2c100f6229b231d6b255e36d1789c25d92285/src/vs/workbench/api/common/extHost.api.impl.ts#L1018
+                `,
+            );
+        }
+
+        // const content = await readFile(fileName);
+
+        return new TextDocumentImpl(fileName);
+    }
+
+    async showTextDocument(
+        document: TextDocument,
+        column?: ViewColumn,
+        preserveFocus?: boolean,
+    ) {
+        console.log(
+            `🌟 EDITOR(${document.uri}) showing... at column=${column} with`,
+            { preserveFocus },
         );
     }
 
-    // const content = await readFile(fileName);
+    // #hack
+    private _onDidCloseTextDocumentEmitter =
+        new EventEmitterImpl<TextDocument>();
 
-    return new TextDocumentImpl(fileName);
+    onDidCloseTextDocument: Event<TextDocument> =
+        this._onDidCloseTextDocumentEmitter.event;
+
+    getConfiguration(
+        section?: string,
+        scope?: ConfigurationScope | null,
+    ): WorkspaceConfiguration {
+        unusedWowo(section, scope);
+        return new WorkspaceConfigurationImpl();
+    }
 }
 
-export async function showTextDocument(
-    document: TextDocument,
-    column?: ViewColumn,
-    preserveFocus?: boolean,
-) {
-    console.log(
-        `🌟 EDITOR(${document.uri}) showing... at column=${column} with`,
-        { preserveFocus },
-    );
-}
-
-// #hack
-const _onDidCloseTextDocumentEmitter = new EventEmitterImpl<TextDocument>();
-
-export const onDidCloseTextDocument: Event<TextDocument> =
-    _onDidCloseTextDocumentEmitter.event;
-
-export function getConfiguration(
-    section?: string,
-    scope?: ConfigurationScope | null,
-): WorkspaceConfiguration {
-    unusedWowo(section, scope);
-    return new WorkspaceConfigurationImpl();
-}
+export default new WorkspaceImpl();
