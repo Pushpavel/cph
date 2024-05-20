98c366164e550ad44a88fb9a6f8d562763425596
Mechi <mechisama1@gmail.com>
2024-05-20 22:12:16 +0530

    handle multiple `/` when concating paths

--- others/vscode/src/api/webview/frontend/index.ts
+++ others/vscode/src/api/webview/frontend/index.ts
@@ -1,9 +1,9 @@
 function acquireVsCodeApi() {
     const ws = new WebSocket(
-        window.location.href.replace('http', 'ws') +
-            '/' +
-            (window as any).viewid +
-            '/messages',
+        `ws://${window.location.host}${window.location.pathname.replace(
+            /\/+$/,
+            '',
+        )}/${(window as any).viewid}/messages`,
         [],
     );
     const connected = new Promise<void>((r) => {
